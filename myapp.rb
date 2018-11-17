e4d2e070d8debeb4e1a64d17a737414680d37317
Juan Marco <jmmarco@gmail.com>
2018-11-17 20:23:31 -0300

    Add erb into app

--- myapp.rb
+++ myapp.rb
@@ -1,11 +1,15 @@
 require 'sinatra'
 
 get '/' do
-  "Hey it's Sinatra! The time on the server is #{Time.now}"
+  erb :home
 end
 
 get '/about' do
   "About section"
+end
+
+get '/greet/:name' do
+  "Hello #{params[:name]}, long time no see!"
 end
 
 
@@ -15,4 +19,14 @@ end
 
 get '/random' do
   "The number is #{rand(1..7)}"
+end
+
+get '/square/:number' do
+  num = params[:number]
+
+  "Yaasss #{num.to_i ** 2}"
+end
+
+get '/welcome' do
+  "<h3>Welcome to <a href='http://sinatrarb.com/'>Sinatra</a>!</h3>"
 end
